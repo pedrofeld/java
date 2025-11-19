32db18f783f4e1bf44a4d111ef49f7f298b4e766
Pedro Feld <pedro.gael.feld@gmail.com>
2025-11-19 20:16:25 -0300

    feat: realizando atv de calcular IMC

---
+++ projetoinicial/src/empresa/Atividades.java
@@ -0,0 +1,20 @@
+package empresa;
+
+import java.util.Scanner;
+
+public class Atividades {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		
+		System.out.println("Digite o peso e a altura:");
+		
+		int peso = teclado.nextInt();
+		double altura = teclado.nextDouble();
+		
+		double imc = peso / (altura * altura);
+		
+		System.out.printf("IMC: %.2f ", imc);
+	}
+
+}
