0faa0a06719df3034d6dc9502fa70d7ef774726a
Pedro Feld <pedro.gael.feld@gmail.com>
2025-11-20 10:25:14 -0300

    improve: redução de código utilizando um operador ternário

--- projetoinicial/src/empresa/PalpiteJogo.java
+++ projetoinicial/src/empresa/PalpiteJogo.java
@@ -10,15 +10,23 @@ public class PalpiteJogo {
 		System.out.println("Digite seu palpite:");
 		
 		int palpite = teclado.nextInt();
-		
 		int valorCorreto = 10000;
+		String msg;		
 		
 		while(palpite != valorCorreto) {
+			// através de um operador ternário:
+			msg = palpite>valorCorreto?"Um pouco menos...":"Um pouco mais...";
+			
+			System.out.println(msg);
+			
+			/*
 			if(palpite > valorCorreto) {
 				System.out.println("Um pouco menos...");
 			} else {
 				System.out.println("Um pouco mais...");
 			}
+			*/
+			
 			System.out.println("Digite outro palpite:");
 			palpite = teclado.nextInt();
 		}
